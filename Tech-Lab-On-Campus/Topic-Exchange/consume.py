db2eeb0c9ce12d7344377fc2d4196aef70dc3ba7
Uday Talasila <talasilauat@gmail.com>
2024-03-20 19:52:11 -0400

    consume

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -24,6 +24,7 @@ def main(sector: str, queueName: str) -> None:
     #                       WRITE CODE HERE!!!
     #
     
+    bindingKey = f"Stock.*.{sector}"
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
     
@@ -35,5 +36,7 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+    sector = sys.argv[1]
+    queue = sys.argv[2]
 
     sys.exit(main(sector,queue))
---
+++ Tech-Lab-On-Campus/Topic-Exchange/solution/consumer_sol.py
@@ -0,0 +1,45 @@
+import pika
+import os
+from consumer_interface import mqConsumerInterface
+import json
+class mqConsumer(mqConsumerInterface):
+
+    def __init__(self, binding_key, exchange_name, queue_name):
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+
+        self.setupRMQConnection()
+    
+
+
+    def setupRMQConnection(self):
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+        exchange = self.channel.exchange_declare(exchange=self.exchange_name)
+        self.channel.queue_bind(
+            queue= self.queue_name,
+            routing_key= self.binding_key,
+            exchange=self.exchange_name,
+        )
+        self.channel.basic_consume(self.queue_name, self.on_message_callback, auto_ack=False)
+
+    def on_message_callback(self, channel, method_frame, header_frame, body):
+        message = json.loads(body) 
+        channel.basic_ack(method_frame.delivery_tag, False)
+        print(message)
+        # channel.close()
+
+    def startConsuming(self):
+        print("[*] Waiting for messages. To exit press CTRL+C")
+        self.channel.start_consuming()  
+
+    def __del__(self):
+        print("Closing RMQ connection on destruction")
+        self.channel.close()
+        self.connection.close()
+        
+
+
